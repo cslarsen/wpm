3606b01452e30865355333db8bca157ad999b746
Christian Stigen Larsen <csl@csl.name>
2017-06-26 16:29:48 +0200

    Bumps to v1.14

--- src/wpm/__init__.py
+++ src/wpm/__init__.py
@@ -8,5 +8,5 @@ Measures your typing speed in words per minute (WPM).
 __author__ = "Christian Stigen Larsen"
 __copyright__ = "Copyright 2017 Christian Stigen Larsen"
 __license__ = "GNU GPL v3 or later"
-__version__ = "1.14"
+__version__ = "1.15"
 
