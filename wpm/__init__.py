91cfc319a5f218395c1df1f11cdd1a57d6e18bd2
Christian Stigen Larsen <csl@csl.name>
2017-08-06 11:42:03 +0200

    Bumps version

--- wpm/__init__.py
+++ wpm/__init__.py
@@ -8,4 +8,4 @@ Measures your typing speed in words per minute (WPM).
 __author__ = "Christian Stigen Larsen"
 __copyright__ = "Copyright 2017 Christian Stigen Larsen"
 __license__ = "GNU GPL v3 or later"
-__version__ = "1.19"
+__version__ = "1.20"
