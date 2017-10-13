ac04e82f013122c6d48ae206540cdab035e2f001
Christian Stigen Larsen <csl@csl.name>
2017-10-13 22:48:44 +0200

    Adds default header

--- wpm/__main__.py
+++ wpm/__main__.py
@@ -1,3 +1,13 @@
+# -*- encoding: utf-8 -*-
+
+"""
+This file is part of the wpm software.
+Copyright 2017 Christian Stigen Larsen
+
+Distributed under the GNU GPL v3 or later. See the file LICENSE.txt for the
+full license text. This software makes use of open source software.
+"""
+
 import wpm.commandline
 
 if __name__ == "__main__":
--- wpm/game.py
+++ wpm/game.py
@@ -3,6 +3,12 @@
 
 """
 Measures your typing speed in words per minute (WPM).
+
+This file is part of the wpm software.
+Copyright 2017 Christian Stigen Larsen
+
+Distributed under the GNU GPL v3 or later. See the file LICENSE.txt for the
+full license text. This software makes use of open source software.
 """
 
 import codecs
