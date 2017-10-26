bbed63461349cbbd1e7824c6e97997682b153eb3
Christian Stigen Larsen <csl@csl.name>
2017-10-26 23:56:10 +0200

    Tunes colors

--- wpm/game.py
+++ wpm/game.py
@@ -45,7 +45,7 @@ class Screen(object):
             curses.init_pair(3, 237, 0)
 
             # Normal text
-            curses.init_pair(4, 195, 0)
+            curses.init_pair(4, 230, 0)
         else:
             curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_RED)
             curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
