f14b099f1cbbfe3565973d2f3d3d11073e668d0a
Christian Stigen Larsen <csl@csl.name>
2017-10-29 19:11:42 +0100

    Sets non-256 color palette

--- wpm/game.py
+++ wpm/game.py
@@ -88,9 +88,35 @@ class Screen(object):
             curses.init_pair(9, 230, 197)
 
         else:
+            bg = curses.COLOR_BLACK
+
+            # Incorrect
             curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_RED)
-            curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
-            curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+
+            # Status
+            curses.init_pair(2, curses.COLOR_CYAN, bg)
+
+            # Done text
+            curses.init_pair(3, curses.COLOR_WHITE, bg)
+
+            # Normal text
+            curses.init_pair(4, curses.COLOR_WHITE, bg)
+
+            # UNUSED
+            curses.init_pair(5, curses.COLOR_WHITE, bg)
+
+            # Author
+            curses.init_pair(6, curses.COLOR_WHITE, bg)
+
+            # Edit text and info
+            curses.init_pair(7, curses.COLOR_WHITE, bg)
+
+            # Background color
+            curses.init_pair(8, bg, bg)
+
+            # Score highlight
+            curses.init_pair(9, curses.COLOR_YELLOW, curses.COLOR_RED)
+
 
         self.window = curses.newwin(curses.LINES, curses.COLS, 0, 0)
         self.window.keypad(True)
