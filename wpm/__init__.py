79f7c2ac09ff77f112d62ddc0bffdf21525dd1ba
Christian Stigen Larsen <csl@csl.name>
2018-03-24 19:41:13 +0100

    Bumps to v1.36

--- wpm/__init__.py
+++ wpm/__init__.py
@@ -9,9 +9,11 @@ Copyright 2017, 2018 Christian Stigen Larsen
 
 Distributed under the GNU GPL v3 or later. See the file LICENSE.txt for the
 full license text. This software makes use of open source software.
+
+The quotes database is *not* covered by the GPL!
 """
 
 __author__ = "Christian Stigen Larsen"
 __copyright__ = "Copyright 2017, 2018 Christian Stigen Larsen"
 __license__ = "GNU GPL v3 or later"
-__version__ = "1.35"
+__version__ = "1.36"
