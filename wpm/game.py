e48117d42ea8a78fb6ee94590413c4af62ed4538
Christian Stigen Larsen <csl@csl.name>
2018-04-21 16:38:01 +0200

    Cleans up pylint notices

--- wpm/game.py
+++ wpm/game.py
@@ -35,7 +35,7 @@ def word_wrap(text, width):
         lengths.append(end)
         text = text[end + 1:]
 
-    if len(text) > 0:
+    if text:
         lengths.append(len(text))
 
     return lengths
@@ -95,12 +95,12 @@ class Screen(object):
         if curses.LINES < 12:
             curses.endwin()
             raise wpm.error.WpmError(
-                    "wpm requires at least 12 lines in your display")
+                "wpm requires at least 12 lines in your display")
 
         if curses.COLS < 51:
             curses.endwin()
             raise wpm.error.WpmError(
-                    "wpm requires at least 51 columns in your display")
+                "wpm requires at least 51 columns in your display")
 
         self.screen.keypad(True)
         curses.noecho()
@@ -114,13 +114,16 @@ class Screen(object):
         self.window.timeout(self.config.window_timeout)
         self.window.bkgd(" ", Screen.COLOR_BACKGROUND)
 
+        # TODO: Get rid of this
+        self.cheight = 0
+
     def set_colors(self):
         """Sets up curses color pairs."""
 
         if os.getenv("TERM").endswith("256color"):
-            bg = self.config.background_color_256
+            bg_col = self.config.background_color_256
             curses.init_pair(Screen.COLOR_AUTHOR, *self.config.author_color_256)
-            curses.init_pair(Screen.COLOR_BACKGROUND, bg, bg)
+            curses.init_pair(Screen.COLOR_BACKGROUND, bg_col, bg_col)
             curses.init_pair(Screen.COLOR_CORRECT, *self.config.correct_color_256)
             curses.init_pair(Screen.COLOR_HISCORE, *self.config.score_highlight_color_256)
             curses.init_pair(Screen.COLOR_INCORRECT, *self.config.incorrect_color_256)
@@ -128,9 +131,9 @@ class Screen(object):
             curses.init_pair(Screen.COLOR_QUOTE, *self.config.quote_color_256)
             curses.init_pair(Screen.COLOR_STATUS, *self.config.status_color_256)
         else:
-            bg = self.config.background_color
+            bg_col = self.config.background_color
             curses.init_pair(Screen.COLOR_AUTHOR, *self.config.author_color)
-            curses.init_pair(Screen.COLOR_BACKGROUND, bg, bg)
+            curses.init_pair(Screen.COLOR_BACKGROUND, bg_col, bg_col)
             curses.init_pair(Screen.COLOR_CORRECT, *self.config.correct_color)
             curses.init_pair(Screen.COLOR_HISCORE, *self.config.score_highlight_color)
             curses.init_pair(Screen.COLOR_INCORRECT, *self.config.incorrect_color)
@@ -150,12 +153,14 @@ class Screen(object):
 
     @staticmethod
     def is_escape(key):
+        """Checks for escape key."""
         if len(key) == 1:
             return ord(key) == curses.ascii.ESC
         return False
 
     @staticmethod
     def is_backspace(key):
+        """Checks for backspace key."""
         if len(key) > 1:
             return key == "KEY_BACKSPACE"
         elif ord(key) in (curses.ascii.BS, curses.ascii.DEL):
@@ -163,6 +168,8 @@ class Screen(object):
         return False
 
     def get_key(self):
+        """Gets a single key stroke."""
+        # pylint: disable=method-hidden
         # Install a suitable get_key based on Python version
         if sys.version_info[0:2] >= (3, 3):
             self.get_key = self.get_key_py33
@@ -171,10 +178,11 @@ class Screen(object):
         return self.get_key()
 
     def get_key_py33(self):
+        """Python 3.3+ implementation of get_key."""
         try:
             # Curses in Python 3.3 handles unicode via get_wch
             key = self.window.get_wch()
-            if type(key) == int:
+            if isinstance(key, int):
                 if key == curses.KEY_LEFT:
                     return "KEY_LEFT"
                 elif key == curses.KEY_RIGHT:
@@ -183,8 +191,7 @@ class Screen(object):
                     return "KEY_RESIZE"
                 elif key == curses.KEY_BACKSPACE:
                     return "KEY_BACKSPACE"
-                else:
-                    return None
+                return None
             return key
         except curses.error:
             return None
@@ -192,27 +199,27 @@ class Screen(object):
             raise
 
     def get_key_py27(self):
+        """Python 2.7 implementation of get_key."""
         try:
             key = self.window.getkey()
 
             # Start of UTF-8 multi-byte character?
             if ord(key[0]) & 0x80:
-                s = key[0]
-                f = ord(key[0]) << 1
-                while f & 0x80:
-                    f <<= 1
-                    s += self.window.getkey()[0]
-                return s.decode("utf-8")
+                multibyte = key[0]
+                cont_bytes = ord(key[0]) << 1
+                while cont_bytes & 0x80:
+                    cont_bytes <<= 1
+                    multibyte += self.window.getkey()[0]
+                return multibyte.decode("utf-8")
 
-            if type(key) == int:
+            if isinstance(key, int):
                 if key == curses.KEY_LEFT:
                     return "KEY_LEFT"
                 elif key == curses.KEY_RIGHT:
                     return "KEY_RIGHT"
                 elif key == curses.KEY_RESIZE:
                     return "KEY_RESIZE"
-                else:
-                    return None
+                return None
             return key.decode("ascii")
         except KeyboardInterrupt:
             raise
@@ -220,6 +227,7 @@ class Screen(object):
             return None
 
     def column(self, y, x, width, text, attr=None, left=True):
+        """Writes text to screen in coumns."""
         lengths = word_wrap(text, width)
         for y, length in enumerate(lengths, y):
             if left:
@@ -230,7 +238,8 @@ class Screen(object):
         return len(lengths)
 
     def update(self, browse, head, quote, position, incorrect, author, title,
-            typed, wpm, average):
+               typed, cur_wpm, average):
+        """Updates the screen."""
 
         if self.config.max_quote_width > 0:
             quote_columns = min(curses.COLS, self.config.max_quote_width)
@@ -243,7 +252,7 @@ class Screen(object):
 
         # Show header
         self.window.addstr(0, 0, pad_right(head, curses.COLS),
-                Screen.COLOR_STATUS)
+                           Screen.COLOR_STATUS)
 
         if browse:
             # Display quote
@@ -265,14 +274,14 @@ class Screen(object):
                                         Screen.COLOR_AUTHOR,
                                         False)
             if browse >= 2:
-                typed = "You scored %.1f wpm%s " % (wpm, "!" if wpm > average
-                        else ".")
+                typed = "You scored %.1f wpm%s " % (cur_wpm, "!" if
+                                                    cur_wpm > average else ".")
             else:
                 typed = ""
             typed += "Use arrows/space to browse, esc to quit, or start typing."
         elif position < len(quote):
             color = (Screen.COLOR_CORRECT if incorrect == 0 else
-                    Screen.COLOR_INCORRECT)
+                     Screen.COLOR_INCORRECT)
             typed = "> " + typed
 
             if position + incorrect < len(quote):
@@ -287,11 +296,11 @@ class Screen(object):
             self.window.move(self.cheight, 0)
             self.window.clrtoeol()
             self.window.addstr(self.cheight, 0, typed.encode("utf-8"),
-                    Screen.COLOR_PROMPT)
+                               Screen.COLOR_PROMPT)
         if browse > 1:
             # If done, highlight score
-            self.window.chgat(self.cheight, 11, len(str("%.1f" % wpm)),
-                    Screen.COLOR_HISCORE)
+            self.window.chgat(self.cheight, 11, len(str("%.1f" % cur_wpm)),
+                              Screen.COLOR_HISCORE)
 
         # Move cursor to current position in text before refreshing
         if browse < 1:
@@ -301,9 +310,11 @@ class Screen(object):
             self.window.move(2, 0)
 
     def clear(self):
+        """Clears the screen."""
         self.window.clear()
 
     def deinit(self):
+        """Deinitializes curses."""
         curses.nocbreak()
         self.screen.keypad(False)
         curses.echo()
@@ -311,6 +322,7 @@ class Screen(object):
 
 
 class Game(object):
+    """The main game runner."""
     def __init__(self, quotes, stats):
         self.config = wpm.config.Config()
         self.stats = stats
@@ -335,23 +347,29 @@ class Game(object):
     def __enter__(self):
         return self
 
-    def __exit__(self, type, value, tb):
+    def __exit__(self, error_type, error_value, error_traceback):
         self.screen.deinit()
-        if type is not None:
+        if error_type is not None:
             return False
         else:
             return self
 
     def set_tab_spaces(self, spaces):
+        """Sets how many spaces a tab should expand to."""
         self.tab_spaces = spaces
 
     def mark_finished(self):
+        """Marks current race as finished."""
         self.stop = time.time()
-        self.stats.add(self.wpm(self.elapsed), self.accuracy,
-                self.quote.text_id, self.quotes.database)
+        self.stats.add(self.wpm(self.elapsed),
+                       self.accuracy,
+                       self.quote.text_id,
+                       self.quotes.database)
+
         self.average = self.stats.average(self.stats.keyboard, last_n=10)
 
     def run(self, to_front=None):
+        """Starts the main game loop."""
         if to_front:
             self.quotes.put_to_front(to_front)
             self.quote = self.quotes._current()
@@ -363,10 +381,16 @@ class Game(object):
             if self.stop is not None:
                 browse = 2
 
-            self.screen.update(browse, self.get_stats(self.elapsed),
-                    self.quote.text, self.position, self.incorrect,
-                    self.quote.author, self.quote.title, self._edit,
-                    self.wpm(self.elapsed), self.average)
+            self.screen.update(browse,
+                               self.get_stats(self.elapsed),
+                               self.quote.text,
+                               self.position,
+                               self.incorrect,
+                               self.quote.author,
+                               self.quote.title,
+                               self._edit,
+                               self.wpm(self.elapsed),
+                               self.average)
 
             key = self.screen.get_key()
             if key is not None:
@@ -376,15 +400,13 @@ class Game(object):
         """Words per minute."""
         if self.start is None:
             return 0
-        else:
-            return min((60.0 * self.position / 5.0) / elapsed, 999)
+        return min((60.0 * self.position / 5.0) / elapsed, 999)
 
     def cps(self, elapsed):
         """Characters per second."""
         if self.start is None:
             return 0
-        else:
-            return min(float(self.position) / elapsed, 99)
+        return min(float(self.position) / elapsed, 99)
 
     @property
     def elapsed(self):
@@ -395,32 +417,35 @@ class Game(object):
         elif self.stop is None:
             # Currently typing
             return time.time() - self.start
-        else:
-            # Done typing
-            return self.stop - self.start
+
+        # Done typing
+        return self.stop - self.start
 
     @property
     def accuracy(self):
+        """Returns typing accuracy."""
         if self.start is None:
             return 0
-        else:
-            n = len(self.quote.text)
-            return float(n) / (n + self.total_incorrect)
+
+        length = len(self.quote.text)
+        return float(length) / (length + self.total_incorrect)
 
     def get_stats(self, elapsed):
+        """Returns the top-bar stats line."""
         keyboard = self.stats.keyboard
         if keyboard is None:
             keyboard = "Unspecified"
 
         return "%5.1f wpm %4.1f cps %5.2fs %5.1f%% acc %5.1f avg wpm - %s" % (
-                self.wpm(elapsed),
-                self.cps(elapsed),
-                elapsed,
-                100.0*self.accuracy,
-                self.average,
-                keyboard)
+            self.wpm(elapsed),
+            self.cps(elapsed),
+            elapsed,
+            100.0*self.accuracy,
+            self.average,
+            keyboard)
 
     def reset(self, direction=0):
+        """Cancels current game."""
         self.start = None
         self.stop = None
 
@@ -438,18 +463,20 @@ class Game(object):
             self.screen.clear()
 
     def resize(self):
+        """Handles a resized terminal."""
         y, x = self.screen.window.getmaxyx()
         self.screen.clear()
         curses.resizeterm(y, x)
 
     def handle_key(self, key):
+        """Dispatches actions based on key and current mode."""
         if key == "KEY_RESIZE":
             self.resize()
             return
 
         # Browse mode
-        if self.start is None or (self.start is not None and self.stop is not
-                None):
+        if self.start is None or (self.start is not None and
+                                  self.stop is not None):
             if key in (" ", "KEY_LEFT", "KEY_RIGHT"):
                 self.reset(direction=-1 if key == "KEY_LEFT" else 1)
                 return
@@ -462,10 +489,10 @@ class Game(object):
             return
 
         if Screen.is_backspace(key):
-            if self.incorrect > 0:
+            if self.incorrect:
                 self.incorrect -= 1
                 self._edit = self._edit[:-1]
-            elif len(self._edit) > 0:
+            elif self._edit:
                 self.position -= 1
                 self._edit = self._edit[:-1]
             return
@@ -474,10 +501,16 @@ class Game(object):
         if self.stop is not None:
             self.reset()
             self.screen.clear()
-            self.screen.update(1, self.get_stats(self.elapsed),
-                    self.quote.text, self.position, self.incorrect,
-                    self.quote.author, self.quote.title, self._edit,
-                    self.wpm(self.elapsed), self.average)
+            self.screen.update(1,
+                               self.get_stats(self.elapsed),
+                               self.quote.text,
+                               self.position,
+                               self.incorrect,
+                               self.quote.author,
+                               self.quote.title,
+                               self._edit,
+                               self.wpm(self.elapsed),
+                               self.average)
 
         # Start recording upon first ordinary key press
         if self.start is None:
