787adf9dc467e86ce2b6730b39e7eab633ef155b
Christian Stigen Larsen <csl@csl.name>
2018-04-21 16:40:19 +0200

    Cleans up pylint notices

--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
-from setuptools import setup
+"""
+Setup script for WPM.
+
+This file is part of the wpm software.
+Copyright 2017, 2018 Christian Stigen Larsen
+
+Distributed under the GNU Affero General Public License (AGPL) v3 or later. See
+the file LICENSE.txt for the full license text. This software makes use of open
+source software.
+
+The quotes database is *not* covered by the AGPL!
+"""
+
 import os
 import runpy
 
+from setuptools import setup
+
 def get_version():
-    filename = os.path.join(os.path.dirname(__file__), "wpm",
-            "__init__.py")
+    """Reads current WPM version from disk."""
+    filename = os.path.join(os.path.dirname(__file__), "wpm", "__init__.py")
     var = runpy.run_path(filename)
     return var["__version__"]
 
