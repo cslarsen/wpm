64811104d7edea0101b7be6269d91bf1b5d3d96f
Christian Stigen Larsen <csl@csl.name>
2018-04-24 22:10:59 +0200

    Memoizes the MacLaurin coefficient function

--- wpm/gauss.py
+++ wpm/gauss.py
@@ -4,30 +4,43 @@ Functions for dealing with the normal distribution.
 
 import math
 
+def memoized(func):
+    """A memoization decorator."""
+    cache = {}
+    def wrap(*args):
+        if args in cache:
+            return cache[args]
+        result = func(*args)
+        cache[args] = result
+        return result
+    return wrap
+
+@memoized
+def c(k):
+    """The MacLaurin coefficient for erf_inv."""
+    if k == 0:
+        return 1
+
+    res = 0
+    for m in range(k):
+        res += (c(m) * c(k - 1 - m)) / float((m + 1) * (2*m + 1))
+
+    return res
+
 def erf_inv(z, steps=10):
-    """Computes the inverse error function thourh a few expansions of its
-    Maclaurin series.
+    """THe inverse error function approximated through its MacLaurin series
+    expansion.
 
     The value z must be inside <-1, 1>.
     """
-    def c(k):
-        """MacLaurin coefficient."""
-        if k == 0:
-            return 1
-
-        res = 0
-        for m in range(k):
-            res += (c(m) * c(k - 1 - m)) / float((m + 1) * (2*m + 1))
-        return res
-
     if not (-1 < z < 1):
         raise ValueError(z)
 
-    out = 0
+    res = 0
     for k in range(steps):
-        out += (c(k) / (2*k + 1)) * (math.sqrt(math.pi) * z / 2.0)**(2*k + 1)
+        res += (c(k) / (2*k + 1)) * (math.sqrt(math.pi) * z / 2.0)**(2*k + 1)
 
-    return out
+    return res
 
 def phi_inv(p):
     """The normal distribution's quantile function z_p / phi^(-1)(p)."""
