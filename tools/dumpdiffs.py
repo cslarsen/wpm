3fbb497a68bd726a48646e81ac5cb6ab340c9ae9
Christian Stigen Larsen <csl@csl.name>
2018-04-26 03:23:16 +0200

    Dumps text length as well

--- tools/dumpdiffs.py
+++ tools/dumpdiffs.py
@@ -5,15 +5,20 @@ from wpm.quotes import Quotes
 from wpm.stats import Stats
 
 if __name__ == "__main__":
-    print("Loading scores and difficulty scores")
+    print("Loading databases")
     stats = Stats.load()
+    quotes = Quotes.load()
     results = list(stats.results().results)
     diffs = Difficulty.load()
 
-    print("Dumping CSV file with (wpm, accuracy, difficulty)")
+    print("Dumping CSV file with (wpm, accuracy, difficulty, length)")
+
+    rows = [(r.wpm, r.accuracy,
+             diffs[r.text_id],
+             len(quotes.from_id(r.text_id).text),
+            ) for r in results]
 
     with open("diffs.csv", "wb") as csvfile:
         csvw = csv.writer(csvfile)
-        rows = [(r.wpm, r.accuracy, diffs[r.text_id]) for r in results]
         for row in rows:
             csvw.writerow(row)
